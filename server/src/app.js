11f12441b6b3b43e11e31a72d5913887ac25bcd1
Franz Gollhammer <franz.gollhammer@gmail.com>
2020-12-01 00:58:56 +0100

    Basic server setup & errorhandlers

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const compression = require('compression')
 const morgan = require('morgan')
+const { notFoundErrorHandler, errorHandler } = require('./middelwares')
 
 const app = express()
 
@@ -9,6 +10,10 @@ app.use(morgan('tiny'))
 app.use(compression())
 app.use(helmet())
 app.use(express.json())
+
+// Error handling
+app.use(notFoundErrorHandler)
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
   res.json({
